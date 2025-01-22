9e4ffc46f34fb806c58f85e9afab7a1fe6ffe8f2
amadeusdotpng <amadeus8182@gmail.com>
2025-01-22 17:48:24 -0600

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,312 @@
+use winit::{
+    event::*,
+    event_loop::EventLoop,
+    keyboard::{KeyCode, PhysicalKey},
+    window::{WindowBuilder, Window},
+};
+
+use wgpu::util::DeviceExt;
+
+struct Renderer<'a> {
+    surface: wgpu::Surface<'a>,
+    size: winit::dpi::PhysicalSize<u32>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+
+    vertex_buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer,
+
+    // TODO:
+    // boids: Boids
+    // boids_buffer: wgpu::Buffer,
+
+    render_pipeline: wgpu::RenderPipeline,
+
+    window: &'a Window,
+}
+
+const VERTICES: &[[f32; 3]] = &[
+    [ 0.00000,  1.00000, 0.0],
+    [-0.55557, -0.83147, 0.0],
+    [ 0.55557, -0.83147, 0.0],
+];
+
+const INDICES: &[u16] = &[
+    0, 1, 2
+];
+
+impl<'a> Renderer<'a> {
+    async fn new(window: &'a Window) -> Renderer<'a> {
+        let size = window.inner_size();
+
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::PRIMARY,
+            ..Default::default()
+        });
+
+        let surface = instance.create_surface(window).unwrap();
+
+        let adapter = instance.request_adapter(
+            &wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            }
+        ).await.unwrap();
+
+        let (device, queue) = adapter.request_device(
+            &wgpu::DeviceDescriptor {
+                required_features: wgpu::Features::empty(),
+                required_limits: wgpu::Limits::default(),
+                label: None,
+                memory_hints: Default::default(),
+            },
+            None,
+        ).await.unwrap();
+
+        let surface_caps = surface.get_capabilities(&adapter);
+
+        let surface_format = surface_caps.formats.iter()
+            .find(|r| r.is_srgb())
+            .copied()
+            .unwrap_or(surface_caps.formats[0]);
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+
+        let vertex_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Vertex Buffer"),
+                contents: bytemuck::cast_slice(VERTICES),
+                usage: wgpu::BufferUsages::VERTEX,
+            }
+        );
+
+        let index_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Index Buffer"),
+                contents: bytemuck::cast_slice(INDICES),
+                usage: wgpu::BufferUsages::INDEX,
+            }
+        );
+
+        let pipeline_layout = device.create_pipeline_layout(
+            &wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            }
+        );
+
+        let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+
+        const VERTEX_ATTRIBS: [wgpu::VertexAttribute; 1] = wgpu::vertex_attr_array![0 => Float32x3];
+        let render_pipeline = device.create_render_pipeline(
+            &wgpu::RenderPipelineDescriptor {
+                label: Some("Render Pipeline"),
+                layout: Some(&pipeline_layout),
+                vertex: wgpu::VertexState {
+                    module: &shader,
+                    entry_point: "vs_main",
+                    compilation_options: wgpu::PipelineCompilationOptions::default(),
+                    buffers: &[
+                        wgpu::VertexBufferLayout {
+                            array_stride: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                            step_mode: wgpu::VertexStepMode::Vertex,
+                            attributes: &VERTEX_ATTRIBS,
+                        },
+                    ],
+                },
+                fragment: Some(wgpu::FragmentState {
+                    module: &shader,
+                    entry_point: "fs_main",
+                    compilation_options: wgpu::PipelineCompilationOptions::default(),
+                    targets: &[
+                        Some(wgpu::ColorTargetState {
+                            format: config.format,
+                            blend: Some(wgpu::BlendState::REPLACE),
+                            write_mask: wgpu::ColorWrites::ALL,
+                        }),
+                    ],
+                }),
+                primitive: wgpu::PrimitiveState {
+                    topology: wgpu::PrimitiveTopology::TriangleList,
+                    strip_index_format: None,
+                    front_face: wgpu::FrontFace::Ccw,
+                    cull_mode: Some(wgpu::Face::Back),
+                    polygon_mode: wgpu::PolygonMode::Fill,
+                    unclipped_depth: false,
+                    conservative: false,
+                },
+                depth_stencil: None,
+                multisample: wgpu::MultisampleState {
+                    count: 1,
+                    mask: !0,
+                    alpha_to_coverage_enabled: false,
+                },
+                multiview: None,
+                cache: None,
+            }
+        );
+
+
+        Self {
+            surface,
+            size,
+            device,
+            queue,
+            config,
+            render_pipeline,
+
+            vertex_buffer,
+            index_buffer,
+
+            // boids
+            // boids_buffer,
+
+            window,
+        }
+    }
+
+    pub fn window(&self) -> &Window {
+        &self.window
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    fn update(&mut self) {
+    
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        // TODO:
+        false
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+        let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+
+        let mut encoder = self.device.create_command_encoder(
+            &wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder")
+            }
+        );
+
+        let mut render_pass = encoder.begin_render_pass(
+            &wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[
+                    Some(wgpu::RenderPassColorAttachment {
+                        view: &view,
+                        resolve_target: None,
+                        ops: wgpu::Operations {
+                            load: wgpu::LoadOp::Clear(wgpu::Color {
+                                r: 0.009021491898012131,
+                                g: 0.009021491898012131,
+                                b: 0.023103556157921437,
+                                a: 1.0,
+                            }),
+                            store: wgpu::StoreOp::Store,
+                        }
+                    })
+                ],
+                depth_stencil_attachment: None,
+                occlusion_query_set: None,
+                timestamp_writes: None,
+            }
+        );
+
+        render_pass.set_pipeline(&self.render_pipeline);
+        render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+
+        render_pass.draw(0..VERTICES.len() as u32, 0..1);
+
+        drop(render_pass);
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+
+        output.present();
+
+        Ok(())
+    }
+}
+
+pub async fn run() {
+    env_logger::init();
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new()
+        .with_decorations(false)
+        .build(&event_loop).unwrap();
+
+    let mut renderer = Renderer::new(&window).await;
+    let mut surface_configured = false;
+
+    let _ = event_loop.run(move |event, control_flow| {
+        if let Event::WindowEvent { window_id, ref event } = event {
+            if window_id != renderer.window().id() { return }
+            if renderer.input(event) { return }
+
+            renderer.update();
+            match event {
+                WindowEvent::CloseRequested
+                | WindowEvent::KeyboardInput { 
+                    event: KeyEvent {
+                        state: ElementState::Pressed,
+                        physical_key: PhysicalKey::Code(KeyCode::Escape),
+                        ..
+                    },
+                    ..
+                } => control_flow.exit(),
+
+                WindowEvent::Resized(physical_size) => {
+                    log::info!("physical_size: {physical_size:?}");
+                    surface_configured = true;
+                    renderer.resize(*physical_size);
+                },
+
+                WindowEvent::RedrawRequested => {
+                    renderer.window().request_redraw();
+
+                    if !surface_configured { return; }
+
+                    match renderer.render() {
+                        Ok(_) => {}
+                                                    // reconfigure the surface if it's lost or outdated
+                        Err(
+                            wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated,
+                        ) => renderer.resize(renderer.size),
+
+                        // system is out of memory so quit
+                        Err(wgpu::SurfaceError::OutOfMemory) => {
+                            log::error!("OutOfMemory");
+                            control_flow.exit();
+                        }
+
+                        // happens when frame takes too long to present
+                        Err(wgpu::SurfaceError::Timeout) => {
+                            log::warn!("Surface Timeout");
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
+    });
+}
